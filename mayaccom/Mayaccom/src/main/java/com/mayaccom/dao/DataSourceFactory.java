17cd15d62b62f4f71f0c05b2b431e3ddefce20e3
zhummer <zita.hummer@gmail.com>
2018-03-17 17:07:35 +0000

    Adding main class

---
+++ mayaccom/Mayaccom/src/main/java/com/mayaccom/dao/DataSourceFactory.java
@@ -0,0 +1,19 @@
+import javax.sql.DataSource;
+import javax.sql.XADataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlXADataSource;
+
+@Configuration
+public class DataSourceFactory {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return DataSourceBuilder.create().type(MysqlXADataSource.class).build();
+    }
+}
